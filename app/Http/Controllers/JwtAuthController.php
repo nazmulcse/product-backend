fb55a98d33f317dd1d3c0c443f4aba75d7319027
Md. Nazmul Hasan <nazmul.hasan@technovista.com.bd>
2020-12-15 16:03:37 +0600

    Initial commit

---
+++ app/Http/Controllers/JwtAuthController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use AppHttpRequestsRegisterAuthRequest;
+use TymonJWTAuthExceptionsJWTException;
+use SymfonyComponentHttpFoundationResponse;
+use JWTAuth;
+use Validator;
+use AppUser;
+use IlluminateHttpRequest;
+use PhpParser\Node\Stmt\TryCatch;
+use Response;
+use Illuminate\Support\Facades\Auth;
+
+class JwtAuthController extends Controller
+{
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['auth/login']]);
+    }
+
+
+    public function register(Request $request)
+    {
+        try{
+            $validator = Validator::make($request->all(), 
+            [ 
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required', 
+            ]);  
+
+            if ($validator->fails()) {  
+                return response()->json(['error'=>$validator->errors()], 401); 
+            }   
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = bcrypt($request->password);
+            $user->save();
+            return response()->json([
+                'success' => true,
+                'data' => $user
+            ], 200);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage()
+            ], 500);
+        }
+        
+    }
+
+    public function login(Request $request)
+    {
+        try{
+            $credentials = $request->only('email', 'password');
+            $token = null;
+            if (! $token = auth()->attempt($credentials)) {
+                return response()->json(['success' => false, 'message' => 'Unauthorized'], 401);
+            }
+
+            return $this->respondWithToken($token);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+     /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'success' => true,
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60
+        ]);
+    }
+
+    public function getUser(){
+        try{
+            return response()->json([
+                'success' => true,
+                'data' => User::all()
+            ], 200);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage()
+            ], 500);
+        }
+    }
+}
---
+++ app/Http/Middleware/Authenticate.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Exception;
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Contracts\Auth\Middleware\AuthenticatesRequests;
+use Closure;
+
+class Authenticate extends Middleware
+{
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string[]  ...$guards
+     * @return mixed
+     *
+     * @throws \Illuminate\Auth\AuthenticationException
+     */
+    public function handle($request, Closure $next, ...$guards)
+    {
+        if($this->authenticate($request, $guards) == 'authentication_failed'){
+            return response()->json(['error'=>'Unauthorized'],401);
+        }
+
+        return $next($request);
+    }
+
+    /**
+     * Determine if the user is logged in to any of the given guards.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  array  $guards
+     * @return void
+     *
+     * @throws \Illuminate\Auth\AuthenticationException
+     */
+    protected function authenticate($request, array $guards)
+    {
+        if (empty($guards)) {
+            $guards = [null];
+        }
+
+        foreach ($guards as $guard) {
+            if ($this->auth->guard($guard)->check()) {
+                return $this->auth->shouldUse($guard);
+            }
+        }
+
+        return 'authentication_failed';
+    }
+}
